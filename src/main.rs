4a481498629ddf10cc6076273958a7429cf7f976
Nazim Lachter <nazim@blueforest.cc>
2023-08-23 21:38:06 +0200

    tiled -> image

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,8 @@ use bevy_ecs_tilemap::prelude::*;
 //mod map;
 //mod time;
 //mod world;
-mod tiled;
 
-const INTERVAL: u64 = 1;
+//const INTERVAL: u64 = 1;
 
 pub type Position = (usize, usize);
 
@@ -51,7 +50,6 @@ async fn main() {
           }),
   )
   .add_plugins(TilemapPlugin)
-  .add_plugins(tiled::TiledMapPlugin)
   .add_systems(Startup, startup)
   .add_systems(Update, camera_movement)
   .run();
@@ -84,14 +82,88 @@ async fn main() {
   }*/
 }
 
+const QUADRANT_SIDE_LENGTH: u32 = 576;
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
   commands.spawn(Camera2dBundle::default());
 
-  let map_handle: Handle<tiled::TiledMap> = asset_server.load("island1/map/island1.tmx");
+  let texture_handle: Handle<Image> = asset_server.load("island1/map/island1.png");
 
-  commands.spawn(tiled::TiledMapBundle {
-      tiled_map: map_handle,
+  // In total, there will be `(QUADRANT_SIDE_LENGTH * 2) * (QUADRANT_SIDE_LENGTH * 2)` tiles.
+  let map_size = TilemapSize {
+      x: QUADRANT_SIDE_LENGTH * 2,
+      y: QUADRANT_SIDE_LENGTH * 2,
+  };
+  let quadrant_size = TilemapSize {
+      x: QUADRANT_SIDE_LENGTH,
+      y: QUADRANT_SIDE_LENGTH,
+  };
+
+  let mut tile_storage = TileStorage::empty(map_size);
+  let tilemap_entity = commands.spawn_empty().id();
+  let tilemap_id = TilemapId(tilemap_entity);
+
+  fill_tilemap_rect_color(
+      TileTextureIndex(5),
+      TilePos { x: 0, y: 0 },
+      quadrant_size,
+      Color::rgba(1.0, 0.0, 0.0, 1.0),
+      tilemap_id,
+      &mut commands,
+      &mut tile_storage,
+  );
+
+  fill_tilemap_rect_color(
+      TileTextureIndex(5),
+      TilePos {
+          x: QUADRANT_SIDE_LENGTH,
+          y: 0,
+      },
+      quadrant_size,
+      Color::rgba(0.0, 1.0, 0.0, 1.0),
+      tilemap_id,
+      &mut commands,
+      &mut tile_storage,
+  );
+
+  fill_tilemap_rect_color(
+      TileTextureIndex(5),
+      TilePos {
+          x: 0,
+          y: QUADRANT_SIDE_LENGTH,
+      },
+      quadrant_size,
+      Color::rgba(0.0, 0.0, 1.0, 1.0),
+      tilemap_id,
+      &mut commands,
+      &mut tile_storage,
+  );
+
+  fill_tilemap_rect_color(
+      TileTextureIndex(5),
+      TilePos {
+          x: QUADRANT_SIDE_LENGTH,
+          y: QUADRANT_SIDE_LENGTH,
+      },
+      quadrant_size,
+      Color::rgba(1.0, 1.0, 0.0, 1.0),
+      tilemap_id,
+      &mut commands,
+      &mut tile_storage,
+  );
+
+  let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+  let grid_size = tile_size.into();
+  let map_type = TilemapType::default();
+
+  commands.entity(tilemap_entity).insert(TilemapBundle {
+      grid_size,
+      size: map_size,
+      storage: tile_storage,
+      texture: TilemapTexture::Single(texture_handle),
+      tile_size,
+      map_type: TilemapType::Square,
+      transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
       ..Default::default()
   });
 }
