157b3c4564ee73019665a9893fa35b7857a9bc6b
Nazim Lachter <nazim@blueforest.cc>
2024-10-13 23:59:07 +0200

    adding bars + emoji day/night

--- prototype/src/play/hud/profile.tsx
+++ prototype/src/play/hud/profile.tsx
@@ -35,15 +35,40 @@ export function Profile() {
       <span>Name: {data.name}</span>
       <span>Seed: {data.seed}</span>
       {time && <span>
-        Time: day {time.day} - {addTrailingZero(time.hour)}:{addTrailingZero(time.minute)}
+        Time: day {time.day} - {addTrailingZero(time.hour)}:{addTrailingZero(time.minute)} {
+          (time.hour >= 6 && time.hour < 18) ? "☀️" : "🌙"
+        }
       </span>}
-      {typeof health !== "undefined" && <span>Health: {health}%</span>}
-      {typeof energy !== "undefined" && <span>Energy: {energy}%</span>}
+      {typeof health !== "undefined" && <span>Health: <ProgressBar value={health}/></span>}
+      {typeof energy !== "undefined" && <span>Energy: <ProgressBar value={energy}/></span>}
       {moneyList.length > 0 && (
         <span>
           Money: {moneyList.map((type) => (money[type] + " " + type)).join(", ")}
         </span>
       )}
     </Module>
+  )
+}
+
+function ProgressBar(props: { value: number }) {
+  return (
+    <span
+      style={{
+        width: "100%",
+        height: "1em",
+        backgroundColor: "black",
+        marginLeft: "0.5em",
+      }}
+      title={props.value + "%"}
+    >
+      <span
+        style={{
+          width: props.value + "%",
+          height: "100%",
+          backgroundColor: "green",
+          display: "block",
+        }}
+      />
+    </span>
   )
 }
