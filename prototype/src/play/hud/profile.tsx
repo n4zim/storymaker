72a1918bb9d9c2c24bfdd8fbb9d979d4a7db9750
Nazim Lachter <nazim@blueforest.cc>
2024-10-13 03:51:23 +0200

    adding more game data in profile

--- prototype/src/play/hud/profile.tsx
+++ prototype/src/play/hud/profile.tsx
@@ -4,21 +4,49 @@ import { receive } from "../../server"
 
 export function Profile() {
   const [data, setData] = useState<{ name: string, seed: number } | undefined>()
+  const [time, setTime] = useState<{ day: number, hour: number, minute: number } | undefined>()
+  const [health, setHealth] = useState<number>(0)
+  const [energy, setEnergy] = useState<number>(0)
+  const [money, setMoney] = useState<{ [type: string]: number }>({})
 
   useEffect(() => {
-    return receive<{ name: string, seed: number }>("profile", (message) => {
-      setData(message)
-    })
+    const cancelData = receive("profile", setData)
+    const cancelTime = receive("time", setTime)
+    const cancelHealth = receive("health", setHealth)
+    const cancelEnergy = receive("energy", setEnergy)
+    const cancelMoney = receive("money", setMoney)
+    return () => {
+      cancelData()
+      cancelTime()
+      cancelHealth()
+      cancelEnergy()
+      cancelMoney()
+    }
   }, [])
 
   if(!data) return null
 
+  const moneyList = Object.keys(money)
+
   return (
     <Module>
       <strong>Profile</strong>
-      Name: {data.name}
-      <br />
-      Seed: {data.seed}
+      <span>Name: {data.name}</span>
+      <span>Seed: {data.seed}</span>
+      {time && <span>
+        Time: day {time.day} - {addTrailingZero(time.hour)}:{addTrailingZero(time.minute)}
+      </span>}
+      {health > 0 && <span>Health: {health}%</span>}
+      {energy > 0 && <span>Energy: {energy}%</span>}
+      {moneyList.length > 0 && (
+        <span>
+          Money: {moneyList.map((type) => (money[type] + " " + type)).join(", ")}
+        </span>
+      )}
     </Module>
   )
+}
+
+function addTrailingZero(number: number) {
+  return number < 10 ? "0" + number : number
 }
